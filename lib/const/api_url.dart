1baf0a8fe640b4e8020c0749a28e6fc30a008054
j.sushaltTMVP <j.sushaltTMVP@tnb.com.my>
2024-05-09 08:43:30 +0800

    change api url

--- lib/const/api_url.dart
+++ lib/const/api_url.dart
@@ -1,4 +1,4 @@
 class APIEndPoints {
   static const mainUrl = "https://c2nr0x6n-8080.inc1.devtunnels.ms/";
-  static const serverUrl = "http://117.221.69.35:8080/";
+  // static const mainUrl = "http://117.221.69.35:8080/";
 }
